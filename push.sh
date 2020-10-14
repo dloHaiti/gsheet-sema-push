5bbba6282d0f18986a6bfe805497791c918ee436
Monero Jeanniton <jeanniton.mnr@gmail.com>
2020-10-14 10:08:18 -0400

    fix(push): update script id for all kiosk sheet.

--- push.sh
+++ push.sh
@@ -9,10 +9,26 @@ ls
 clasp setting scriptId 1cVzxPNLIjPWIcAlPqAfhOQxYFvq97eNTw6KTOjjqjMAU9TGTuZeozYb0
 clasp push
 # Corail | 2
+clasp setting scriptId 1ZE0j09botra4iaAhDNdETmKSHBnIZRER9OqE0f645S_T4F4KmUF_8abK
+clasp push
 # Cabaret | 4
+clasp setting scriptId 1HE45EELb6t85SISpo8Q8vAKCiML9FjaavZcUsLMl_lPKfmvMOy67tPmO
+clasp push
 # Santo19 | 5
+clasp setting scriptId 1pf87cykIAysgrjDitvVjtnzR2OklSkWvQSXNcMWbnwDGSOgr78htfWGN
+clasp push
 # Bois9 | 6
+clasp setting scriptId 130rrzgkgZgYpDPHSrRr_fbc8H2Hu9t7JJ-toM_xA7dHTeCbmjhOIzXqU
+clasp push
 # Quartier Morin | 7
+clasp setting scriptId 1m4MGJnT4Xg68Ptoio-Nsjv31dO-x1VTlEmpA20xSK7P9IZrfmDIJZCpm
+clasp push
 # Limonade | 8
+clasp setting scriptId 17m30t0Wao0eI8v8V94xD-oP1x7OiCHlERpJOnelKIXR8KXj8B_aBbT2C
+clasp push
 # Ouanamonthe | 9
-
+clasp setting scriptId 1dYurGJQ5KB_f__yiH-Pd84CojbFvTFvbXHF5SdJLbEsVKDI6yafgmTgj
+clasp push
+# General | 0 (Update it the repo back to general)
+clasp setting scriptId  1iWmlrID4G4RhCnXukmBDEoMdOvat5VYaAMcXGc1spolwsP6KE4XaUCJO
+clasp push
