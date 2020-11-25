efb97ec5f2c5321ab4924130525a8cfb5867148b
Monero Jeanniton <jeanniton.mnr@gmail.com>
2020-11-25 04:33:04 -0500

    refactored(refresh.data): code improvment.

--- actions.refresh.data.js
+++ actions.refresh.data.js
@@ -33,53 +33,40 @@ function loadSaleRangeFromSema(properties) {
     const API_SALE_ENDPOINT = '/sema/site/receipts';
     const sheet = SpreadsheetApp.getActive().getSheetByName('AllSales');
 
-    try {
-        const receipts = _fetch('get', API_SALE_ENDPOINT, properties);
-        // normalize from receipts to sale lines
-        const sales = getSalesFromReceipts(receipts);
-        // Add header to sales output
-        sales.unshift([
-            "Kiosk", "Date", "Customer ID", "Customer Name", "Sales Channel", "SKU", "Qty", "Gallons", "Total", "Credit"
-        ]);
-        // Clear and get range from sheet
-        // getRange(LINE, COLUMN)
-        var range = sheet.clear().getRange(1, 1, sales.length, (sales[0]).length);
-        // update sale range
-        range.setValues(sales);
-        // toast success
-        _toast("Done");
-    } catch (err) {
-        console.log(err.message);
-    }
+    const receipts = _fetch('get', API_SALE_ENDPOINT, properties);
+    // normalize from receipts to sale lines
+    const sales = getSalesFromReceipts(receipts);
+    // Add header to sales output
+    sales.unshift([
+      "Kiosk", "Date", "Customer ID", "Customer Name", "Sales Channel", "SKU", "Qty", "Gallons", "Total", "Credit"
+    ]);
+    // Clear and get range from sheet
+    // getRange(LINE, COLUMN)
+    var range = sheet.clear().getRange(1, 1, sales.length, (sales[0]).length);
+    // update sale range
+    range.setValues(sales);
+    // toast success
+    _toast("Done");
 }
 
 const getSalesFromReceipts = function (receipts) {
   const sales = [];
-  
-//   receipts = reeceipts.map((receipt) => {
-  
-//   });
-  
-  for (let rI = 0, rLen = receipts.length; rI < rLen; rI++) {
-    const receipt = receipts[rI];
-    const items = receipt.receipt_line_items;
-    for (var i = 0, len = items.length; i < len; i++) {
-      let item = items[i];
-      let sale = [
-        receipt.kiosk.name,
-        new Date(receipt.created_at).toDateString(),
-        receipt.customer_account.id,
-        receipt.customer_account.name,
-        receipt.sales_channel.name,
-        item.product.sku,
-        (item.product.sku == "LOANPAYOFF") ? 0 : (parseFloat(item.quantity, 10)||0),
-        (item.product.sku == "LOANPAYOFF") ? 0 : (parseFloat(item.quantity, 10) * item.product.unit_per_product)||0,
-        (item.product.sku == "LOANPAYOFF") ? 0 : (parseFloat(item.price_total, 10)||0),
-        (item.product.sku == "LOANPAYOFF") ? (-1 * parseFloat(receipt.amount_cash, 10)) : parseFloat(receipt.amount_loan, 10)
-      ];
-      sales.push(sale);
-      
-    }
-  }
-  return sales.length ? sales : null;
+  receipts.forEach((receipt) => {
+      receipt.receipt_line_items.forEach((lineItem) => {
+          sales.push([
+              receipt.kiosk_name,
+              new Date(receipt.created_at).toDateString(),
+              receipt.customer_account.id,
+              receipt.customer_account.name,
+              receipt.sales_channel.name,
+              lineItem.product.sku,
+              (lineItem.product.sku == "LOANPAYOFF") ? 0 : (parseFloat(item.quantity, 10)||0),
+              (lineItem.product.sku == "LOANPAYOFF") ? 0 : (parseFloat(item.quantity, 10) * item.product.unit_per_product)||0,
+              (lineItem.product.sku == "LOANPAYOFF") ? 0 : (parseFloat(item.price_total, 10)||0),
+              (lineItem.product.sku == "LOANPAYOFF") ? (-1 * parseFloat(receipt.amount_cash, 10)) : parseFloat(receipt.amount_loan, 10)
+          ]);
+      });
+  });
+
+  return sales;
 }
