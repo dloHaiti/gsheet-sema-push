6654aad198d26b8f754975cbbec907801196b1e5
Monero Jeanniton <jeanniton.mnr@gmail.com>
2020-12-01 02:17:10 -0500

    fix(action.upload.sorties): finalize code.

--- action.upload.sorites.js
+++ action.upload.sorites.js
@@ -8,6 +8,26 @@ function uploadSortiesRange() {
     rowBetween: betweenLine
   });
   // TODO: Use _createExpenseFromSorties
+  const expenses = _createExpenseFromSorties(sorties);
+  // Upload receipts to sema
+  var _expenses = _fetch("POST", API_POST_SALE_ENDPOINT, expenses);
+  // TODO: Test Log uuid of each receipt on success
+  _expenses.forEach(function (expense, index) {
+    // update uuid
+    updatePosition({
+      sheetName: "SORTIES",
+      data: expense.uuid,
+      x: sorties[index].line_number,
+      y: 8
+    });
+    // update updated_at
+    updatePosition({
+      sheetName: "SORTIES",
+      data: expense.updated_at,
+      x: sorties[index].line_number,
+      y: 9
+    });
+  });
 
   console.log(data);
 
