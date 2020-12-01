b62f6e11837c3ff25fd820b4afc119ab20698db0
Monero Jeanniton <jeanniton.mnr@gmail.com>
2020-12-01 04:43:04 -0500

    hotfix(action.upload.sorties): minor error.

--- action.upload.sorites.js
+++ action.upload.sorites.js
@@ -8,7 +8,11 @@ function uploadSortiesRange() {
     rowBetween: betweenLine
   });
   // Getting our sales into receipt
-  const expenses = data.map((expense) => _createExpenseFromSorties(expense));
+  const expenses = data.map((expense) => _createExpenseFromSorties(expense))
+    // filter out empty data lines.
+    .filter((expense) => {
+      return (xpense.created_at ||	expense.notes ||	expense.total ||	expense.notes_2 ||	expense.expense_account_id ||	expense.kiosk_id);
+    });
   // Upload receipts to sema
   var _expenses = _fetch("POST", API_POST_EXPENSE_ENDPOINT, expenses);
   // TODO: Test Log uuid of each receipt on success
